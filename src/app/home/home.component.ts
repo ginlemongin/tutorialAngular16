8712881885eb323307ba46ce956874b95071c510
ginlemongin <davide.morolli@enaipriminifad.org>
2023-07-05 16:32:19 +0200

    w

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -32,6 +32,9 @@ export class HomeComponent {
   housingService: HousingService = inject(HousingService);
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      //this.filteredLocationList = housingLocationList;
+    });
   }
 }
